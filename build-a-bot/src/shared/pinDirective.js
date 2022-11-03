1cbc9dcffae1e4a15f792aab166a771aa99d628a
Daniel <dnl.canedo@gmail.com>
2022-11-02 22:12:31 -0400

    Improve directive

--- build-a-bot/src/shared/pinDirective.js
+++ build-a-bot/src/shared/pinDirective.js
@@ -1,12 +1,10 @@
 /* eslint no-param-reassign: 0 */
-export default {
-  beforeMount(element, binding) {
-    if (binding.arg !== 'position') return;
+export default function (element, binding) {
+  if (binding.arg !== 'position') return;
 
-    Object.keys(binding.modifiers).forEach((modifier) => {
-      element.style[modifier] = '5px';
-    });
+  Object.keys(binding.modifiers).forEach((modifier) => {
+    element.style[modifier] = '5px';
+  });
 
-    element.style.position = 'absolute';
-  },
-};
+  element.style.position = 'absolute';
+}
